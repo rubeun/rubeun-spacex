cfdee5f1d08fe01ba2419ec16f146ca469502ae2
Rubeun Tan <github@rubeun.com>
2022-05-12 15:13:57 -0700

    add relayEnvironment, fetchGraphQL

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { RelayEnvironmentProvider, Environment } from "react-relay/hooks";
+import relayEnvironment from './relay/relayEnvironment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RelayEnvironmentProvider
+      environment={relayEnvironment as unknown as Environment}
+    >
+
+    </RelayEnvironmentProvider>
   );
 }
 
